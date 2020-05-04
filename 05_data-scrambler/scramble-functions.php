5738e745a2fdce1867323478a8530b47c233a101
Pobitro Raj <pobitroraj.07@gmail.com>
2020-05-04 13:41:17 +0600

    data scrambler php project

---
+++ 05_data-scrambler/scramble-functions.php
@@ -0,0 +1,37 @@
+<?php
+
+function displayKey($key) {
+    printf("value = '%s' ", $key);
+}
+
+function scrambleData($originalData, $key) {
+    $original_key = 'abcdefghijklmnopqrstuvwxyz1234567890';
+    $data = '';
+    $length = strlen($originalData);
+    for ($i = 0; $i < $length; $i++) {
+        $currentChar = $originalData[$i];
+        $position = strpos($original_key, $currentChar);
+        if ($position != false) {
+            $data .= $key[$position];
+        } else {
+            $data .= $currentChar;
+        }
+    }
+    return $data;
+}
+
+function decodeData($originalData, $key) {
+    $original_key = 'abcdefghijklmnopqrstuvwxyz1234567890';
+    $data = '';
+    $length = strlen($originalData);
+    for ($i = 0; $i < $length; $i++) {
+        $currentChar = $originalData[$i];
+        $position = strpos($key, $currentChar);
+        if ($position != false) {
+            $data .= $original_key[$position];
+        } else {
+            $data .= $currentChar;
+        }
+    }
+    return $data;
+}
