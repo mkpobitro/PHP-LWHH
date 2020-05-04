485266497aac62165475a66f62b6e651c2e9e6f4
Pobitro Raj <pobitroraj.07@gmail.com>
2020-05-04 12:43:23 +0600

    text mail is working

--- 16_mailing/16.1_text-email.php
+++ 16_mailing/16.1_text-email.php
@@ -4,5 +4,10 @@ $subject = "PHP Test Email";
 $message = "Lorem ipusm \n sit dolor amet";
 $from = "Jhon Gillbart <jhon@gmail.com>";
 $headers = "From: {$from}\r\n";
-echo mail( $to, $subject, $message, $headers );
-// echo "Mail sent";
+
+if(mail( $to, $subject, $message, $headers )){
+    echo "Mail has been sent";
+}else{
+    echo "Failed";
+}
+
