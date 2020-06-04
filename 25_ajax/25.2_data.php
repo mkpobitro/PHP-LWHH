c749bcf4cf321ac3eca772de540b0a1d922fea33
Pobitro Raj <pobitroraj.07@gmail.com>
2020-06-04 13:28:39 +0600

    Ajax - Asynchronus JavaScript & XML

---
+++ 25_ajax/25.2_data.php
@@ -0,0 +1,4 @@
+<?php
+$data = $_GET;
+$data['result'] = 'success';
+echo json_encode( $data, JSON_PRETTY_PRINT );
---
+++ 25_ajax/25.3_data.php
@@ -0,0 +1,4 @@
+<?php
+$data = $_POST;
+$data['result'] = 'success';
+echo json_encode( $data, JSON_PRETTY_PRINT );
